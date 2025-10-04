8b0dd88c9f89734ba9572af5bb7e2d8740a24728
Ajith Kattu <ajithkt@gmail.com>
2025-10-04 06:59:32 -0400

    Day 27: Created app to get tesla news from newsapi.org and send an email

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
+from send_email import send_email
 
-API_KEY = "0a9f8103d7514c88806cb59ae215dc8f"
+
 url=("https://newsapi.org/v2/everything?q=tesla&from=2025-09-04&sortBy=publishedAt&"
      "apiKey=0a9f8103d7514c88806cb59ae215dc8f")
 
@@ -10,7 +11,14 @@ request = requests.get(url)
 # Get Dictionary with data
 content = request.json()
 
+email_content = ""
+
 # Access the article title and description.
 for article in content['articles']:
-     print(article['title'])
-     print(article['description'])
+     email_content = email_content + str(article['title']) + "\n" + str(article['description']) + 2*"\n"
+     # email_content = email_content + article['title'] + "\n" + article['description'] + 2*"\n"
+
+email_content = email_content.encode("utf-8")
+print(email_content)
+send_email(email_content)
+
